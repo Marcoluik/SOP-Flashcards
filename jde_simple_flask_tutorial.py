6d59d7e6168abaf6683dada2ffbb961c4156ecb4
Jacob Debel <jacobdebel@gmail.com>
2024-04-09 18:04:22 +0200

    Sceond example working

--- jde_simple_flask_tutorial.py
+++ jde_simple_flask_tutorial.py
@@ -1,14 +1,14 @@
 # This application is taken from
 # https://www.geeksforgeeks.org/flask-creating-first-simple-application/
-
+# This is the second example. Try it out on. http://127.0.0:5000/hello/Marco
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello World"
+@app.route("/hello/<name>")
+def hello_name(name):
+    return f"Hello {name}! This is working!"
 
 
 if __name__ == "__main__":
