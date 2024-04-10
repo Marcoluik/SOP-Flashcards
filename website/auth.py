a58be597df7a2fe254962e36fac6e39e51f50ed0
Marco <marcokot@icloud.com>
2024-04-10 12:30:40 +0200

    fixet login, new login_user var med user og ikke new user

--- website/auth.py
+++ website/auth.py
@@ -51,7 +51,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password, method="pbkdf2:sha256"))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash("Your account has been created", category="success")
             return redirect(url_for("views.home"))
     return render_template("sign_up.html", user=current_user)
