a96977bd361aa026a605fe2412ba8b7d6d596ff6
Marco <marcokot@icloud.com>
2024-04-10 12:26:33 +0200

    flashcards virker med relationship til deres stacks, man kan også create flashcards og på en simpel måde se dem.

--- website/models.py
+++ website/models.py
@@ -2,6 +2,19 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+class FlashcardStack(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)  
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))  
+    flashcards = db.relationship("Flashcard", backref="stack", lazy=True) 
+
+class Flashcard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(1000), nullable=False)  
+    answer = db.Column(db.String(1000), nullable=False) 
+    stack_id = db.Column(db.Integer, db.ForeignKey("flashcard_stack.id"), nullable=False)  
+
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)    
     data = db.Column(db.String(10000))
@@ -15,3 +28,5 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship("Note")
+    flashcard_stacks = db.relationship("FlashcardStack", backref="user", lazy=True) 
+
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import  login_required, current_user
-from .models import Note
+from .models import Note, Flashcard, FlashcardStack
 from . import db
 import json
 
@@ -33,3 +33,35 @@ def delete_note():
             flash("Note has been deleted", category="success")
             
     return jsonify({})
+    
+@views.route("/flashcards", methods=["GET", "POST"])
+@login_required
+def flashcards():
+    return render_template("flashcards.html", user=current_user)
+
+@views.route("/createflashcards", methods=["GET", "POST"])
+@login_required
+def createflashcards():
+    if request.method == "POST":
+        stack_name = request.form.get("stack_name")
+        question = request.form.get("question")
+        answer = request.form.get("answer")
+
+        stack = FlashcardStack.query.filter_by(title=stack_name, user_id=current_user.id).first()
+
+        if not stack:
+            new_stack = FlashcardStack(title=stack_name, user_id=current_user.id)
+            db.session.add(new_stack)
+            db.session.commit()
+            flash("New flashcard stack created", category="success")
+            stack = new_stack
+            
+        new_flashcard = Flashcard(question=question, answer=answer, stack_id=stack.id)
+        db.session.add(new_flashcard)
+        db.session.commit()
+        flash("New flashcard has been added", category="success")
+
+
+
+    return render_template("createflashcards.html", user=current_user)
+
